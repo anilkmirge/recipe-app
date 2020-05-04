59a995a9ad2515e5d2c65c3b284b01618bcad293
Anil Mirge <manilche@gmail.com>
2020-05-03 23:51:29 -0400

    Updating Recipes, Recipe Details and Footer

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,50 +1,61 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Search from '../Search/Search';
 import RecipeList from '../RecipeList/RecipeList';
 import Footer from '../Footer/Footer';
 
 function Recipes() {
-    const [recipes, setRecipes] = useState([]);
-    const [searchString, setSearchString] = useState('chicken');
+	const savedRecipes = JSON.parse(localStorage.getItem('recipes'));
+	const recipesState =
+		savedRecipes && savedRecipes.length > 0 ? savedRecipes : [];
+	const savedString = localStorage.getItem('savedString');
+	const searchStringState = savedString ? savedString : 'chicken';
+	const [recipes, setRecipes] = useState(recipesState);
+	const [searchString, setSearchString] = useState(searchStringState);
 
-    useEffect(() => {
-        getRecipes(searchString);
-    }, []);
+	useEffect(() => {
+		getRecipes(searchString);
+	}, []);
 
-    async function getRecipes(searchString) {
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
-        await fetch(url)
-            .then((response) => response.json())
-            .then((result) => {
-                if (result.meals && result.meals.length > 0) {
-                    setRecipes(result.meals);
+	async function getRecipes(searchString) {
+		const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
+		await fetch(url)
+			.then((response) => response.json())
+			.then((result) => {
+				if (result.meals && result.meals.length > 0) {
+					setRecipes(result.meals);
+					localStorage.setItem('recipes', JSON.stringify(result.meals));
+					localStorage.setItem('savedString', searchString);
+					console.log(searchString);
+					setSearchString('');
+				} else {
+					alert('Recipe not found');
+					setSearchString('');
+				}
+			})
+			.catch(console.error);
+	}
 
-                    setSearchString('');
-                } else {
-                    alert('Recipe not found');
-                    setSearchString('');
-                }
-            })
-            .catch(console.error);
-    }
+	function handleChange(event) {
+		setSearchString(event.target.value);
+	}
 
-    function handleChange(event) {
-        setSearchString(event.target.value);
-    }
+	function handleSubmit(event) {
+		event.preventDefault();
+		getRecipes(searchString);
+	}
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        getRecipes(searchString);
-    }
-
-    return (
-        <div>
-            <Search handleChange={handleChange} handleSubmit={handleSubmit} searchString={searchString} />
-            <RecipeList recipes={recipes} />
-            {recipes.length > 0 ? <Footer /> : null}
-        </div>
-    );
+	return (
+		<div className='mb-5'>
+			<Search
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+				searchString={searchString}
+			/>
+			<RecipeList recipes={recipes} />
+			{recipes.length > 0 ? <Footer /> : null}
+		</div>
+	);
 }
 
 export default Recipes;
